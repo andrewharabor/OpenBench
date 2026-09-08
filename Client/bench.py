21d6601175adf78979aebbe7d64d3b280113a1f5
Andrew Harabor <andrew.harabor@gmail.com>
2026-09-07 22:44:16 -0700

    working?

--- Client/bench.py
+++ Client/bench.py
@@ -50,8 +50,8 @@ def parse_stream_output(stream):
         line = re.sub(r'[^a-zA-Z0-9 ]+', ' ', line)
 
         # Multiple methods, including Ethereal and Stockfish
-        nps_pattern   = r'(\d+\s+nps)|(nps\s+\d+)|(nodes second\s+\d+)'
-        bench_pattern = r'(\d+\s+nodes)|(nodes\s+\d+)|(nodes searched\s+\d+)'
+        nps_pattern   = r'(nps\s+\d+)'
+        bench_pattern = r'(nodes\s+\d+)'
 
         # Search for and set only once the NPS and Bench values
         re_nps = re.search(nps_pattern, line, re.IGNORECASE)
